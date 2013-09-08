a36002ea6d89bf5a788bdaac75601e4324b2eee0
Chenzhutian <chenzhutian1992@gmail.com>
2013-09-08 23:43:30 +0800

    add qrCode activity

---
+++ src/com/billme/ui/QRCodeActivity.java
@@ -0,0 +1,35 @@
+package com.billme.ui;
+
+import android.os.Bundle;
+
+import com.billme.logic.BillMeActivity;
+
+public class QRCodeActivity extends BaseActivity implements BillMeActivity{
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void refresh(Object... param) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
